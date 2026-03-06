70012662ce7f3652feaf9a5d125b7f892eb5d1b0
PEDRO HENRIQUE DANTAS DUARTE <43049122838@info.local>
2026-03-06 18:57:39 -0300

    funçao exponencial

--- desafio.py
+++ desafio.py
@@ -45,3 +45,20 @@ print("**** Calculando o Número de Euler **** ")
 print("")
 termos = int(input("Digite o máximo de repetições que deseja (o idea seria 20 para ter uma melhor precisão): "))
 print(f"O Resultado do Número de Euler é ≈ {numero_e(termos)}")
+
+#pedro
+#exponencial 
+
+def exponencial (x, termos=50):
+    termo_atual = 1.0
+    resultado = 1
+    
+    for i in range (1, termos):
+        termo_atual = termo_atual * x / i
+        resultado += termo_atual
+        
+    return resultado
+
+x = float(input("digite seu numero: "))
+
+print(exponencial(x))
